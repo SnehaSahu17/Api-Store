eccfce86c1f8c3214ea9ab8a15edd899f088c04b
Sneha Sahu <snehasahu@Snehas-MacBook-Air.local>
2023-03-06 13:03:38 +0530

    ft:modified

--- src/components/ButtonFilter.jsx
+++ src/components/ButtonFilter.jsx
@@ -1,31 +1,37 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
-import useAxios from '../hooks/useAxios'
+import { useEffect } from "react";
+import useAxios from "../hooks/useAxios"
 
-function ButtonFilter() {
+const ButtonFilter = ({ fetchData: fetchAPi }) => {
+  const { fetchData, response: { categories }, loading } = useAxios('categories');
 
-    const {fetchData, response: {categories}, loading} = useAxios(`categories`);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    useEffect(()=> {
-        fetchData();
-    },[]);
+  if(loading) {
+    return (
+      <div className="animate-pulse my-5 inline-block text-center">
+        {[...Array(35).keys()].map(num => (
+          <div key={num} className="h-7 w-20 bg-gray-300 m-1 rounded-sm inline-block"></div>
+        ))}
+      </div>
+    )
+  }
 
-    if(loading){
-        return (
-            <div className='animate-pulse my-5 inline-block text-center'>
-                {[...Array(35).keys()].map( num => (
-                    <div key={num} className="h-7 w-20 bg-gray-300 m-1 rounded-sm inline-block"></div>
-                ))}
-            </div>
-        );
-    }
+  const clickFilterButton = e => {
+    fetchAPi({ params: { category: e.target.value } });
+  }
 
   return (
-    <div className='text-center my-5'>
-        {categories && categories.map(button => (
-            <button key={button}
-            className="bg-pink-400 text-white m-1 py-1 px-2 rounded-sm hover:bg-pink-500">{button}</button>
-        ))}
+    <div className="text-center my-5">
+      {categories && categories.map(button => (
+        <button
+          key={button}
+          className="bg-pink-400 text-white m-1 py-1 px-2 rounded-sm hover:bg-pink-500"
+          onClick={clickFilterButton}
+          value={button}
+        >{button}</button>
+      ))}
     </div>
   )
 }
